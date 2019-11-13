2078030c8b68bcb1240175b33bed56c549d81af3
kj_cho <kj_cho@idealidea.co.kr>
2019-11-13 20:38:42 +0900

    TedPermission 을 이용한 권한 획득 예제

--- app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.tjoeun.a20191113_01_libraries
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -17,6 +20,33 @@ class MainActivity : BaseActivity() {
 
     }
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+
+            var permissionListner = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    var uri = Uri.parse("tel:${phoneNumTxt.text.toString().replace("-", "")}")
+                    var intent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(intent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 설정해야 전화 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListner)
+                .setRationaleMessage("전화 권한을 설정해야 바로 연결 가능합니다.")
+                .setDeniedMessage("설정 > 권한에서 허용 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+        }
+
+
 
     }
 
